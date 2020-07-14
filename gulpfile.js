63c9735ecf268f32abad4c26ab7e9232fed7bbc8
Георгиевский Максим Игоревич <georgievskiy.mi@defo.local>
2020-07-14 16:27:27 +0300

    Init

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 // VARIABLES & PATHS
 
-let preprocessor = 'scss', // Preprocessor (sass, scss, less)
-    fileswatch   = 'html,htm,txt,json,md,woff2', // List of files extensions for watching & hard reload (comma separated)
+let fileswatch   = 'html,htm,txt,json,md,woff2', // List of files extensions for watching & hard reload (comma separated)
     imageswatch  = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
     baseDir      = 'app', // Base directory path without «/» at the end
     online       = true; // If «false» - Browsersync will work offline without internet connection
@@ -17,20 +16,13 @@ let paths = {
 	},
 
 	styles: {
-		src:  baseDir + '/' + preprocessor + '/main.*',
+		src:  baseDir + '/scss/main.*',
 		dest: baseDir + '/css',
 	},
 
 	images: {
 		src:  baseDir + '/img-src/**/*',
 		dest: baseDir + '/img',
-	},
-
-	deploy: {
-		hostname:    'username@yousite.com', // Deploy hostname
-		destination: 'yousite/public_html/', // Deploy destination
-		include:     [/* '*.htaccess' */], // Included files to deploy
-		exclude:     [ '**/Thumbs.db', '**/*.DS_Store' ], // Excluded files from deploy
 	},
 
 	cssOutputName: 'main.min.css',
@@ -41,10 +33,7 @@ let paths = {
 // LOGIC
 
 const { src, dest, parallel, series, watch } = require('gulp');
-const sass         = require('gulp-sass');
 const scss         = require('gulp-sass');
-const less         = require('gulp-less');
-const styl         = require('gulp-stylus');
 const cleancss     = require('gulp-clean-css');
 const concat       = require('gulp-concat');
 const browserSync  = require('browser-sync').create();
@@ -52,8 +41,8 @@ const uglify       = require('gulp-uglify-es').default;
 const autoprefixer = require('gulp-autoprefixer');
 const imagemin     = require('gulp-imagemin');
 const newer        = require('gulp-newer');
-const rsync        = require('gulp-rsync');
 const del          = require('del');
+const normalize    = require('node-normalize-scss');
 
 function browsersync() {
 	browserSync.init({
@@ -73,7 +62,7 @@ function scripts() {
 
 function styles() {
 	return src(paths.styles.src)
-	.pipe(eval(preprocessor)())
+	.pipe(scss())
 	.pipe(concat(paths.cssOutputName))
 	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
@@ -92,23 +81,8 @@ function cleanimg() {
 	return del('' + paths.images.dest + '/**/*', { force: true })
 }
 
-function deploy() {
-	return src(baseDir + '/')
-	.pipe(rsync({
-		root: baseDir + '/',
-		hostname: paths.deploy.hostname,
-		destination: paths.deploy.destination,
-		include: paths.deploy.include,
-		exclude: paths.deploy.exclude,
-		recursive: true,
-		archive: true,
-		silent: false,
-		compress: true
-	}))
-}
-
 function startwatch() {
-	watch(baseDir  + '/**/' + preprocessor + '/**/*', styles);
+	watch(baseDir  + '/**/scss/**/*', styles);
 	watch(baseDir  + '/**/*.{' + imageswatch + '}', images);
 	watch(baseDir  + '/**/*.{' + fileswatch + '}').on('change', browserSync.reload);
 	watch([baseDir + '/**/*.js', '!' + paths.scripts.dest + '/*.min.js'], scripts);
@@ -120,5 +94,4 @@ exports.styles      = styles;
 exports.scripts     = scripts;
 exports.images      = images;
 exports.cleanimg    = cleanimg;
-exports.deploy      = deploy;
 exports.default     = parallel(images, styles, scripts, browsersync, startwatch);
